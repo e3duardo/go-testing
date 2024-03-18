230f63469b47f7988f1774d08e0db89b4f489ecd
Eduardo Santos <eduardo.santos@g.globo>
2024-03-18 10:04:45 -0300

    120. Testing logging users out of our SPA

--- webapp/cmd/api/api-handlers_test.go
+++ webapp/cmd/api/api-handlers_test.go
@@ -184,6 +184,7 @@ func Test_app_refreshUsingCookie(t *testing.T) {
 	}{
 		{"valid cookie", true, testCookie, http.StatusOK},
 		{"invalid cookie", true, badCookie, http.StatusBadRequest},
+		{"no cookie", false, nil, http.StatusUnauthorized},
 	}
 
 	for _, e := range tests {
@@ -200,5 +201,31 @@ func Test_app_refreshUsingCookie(t *testing.T) {
 		if rr.Code != e.expectedStatus {
 			t.Errorf("%s: wrong status code returned; expected %d, but got %d", e.name, e.expectedStatus, rr.Code)
 		}
+	}
+}
+
+func Test_app_deleteRefreshCookie(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/logout", nil)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(app.deleteRefreshCookie)
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusAccepted {
+		t.Errorf("wrong status; expected %d, but got %d", http.StatusAccepted, rr.Code)
+	}
+
+	foundCookie := false
+	for _, c := range rr.Result().Cookies() {
+		if c.Name == "__Host-refresh_token" {
+			foundCookie = true
+			if c.Expires.After(time.Now()) {
+				t.Errorf("cookie expiration in future, and should not be: %v", c.Expires.UTC())
+			}
+
+		}
+	}
+
+	if !foundCookie {
+		t.Error("__Host-refresh_token cookie not found")
 	}
 }
